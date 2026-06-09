d554f0e808a9c9a05540fdb2852d690ef16607b8
tahiamubin <tahia.mubin1@gmail.com>
2026-06-10 01:17:37 +0600

    payment system done

--- src/app/api/checkout_sessions/route.js
+++ src/app/api/checkout_sessions/route.js
@@ -1,31 +1,38 @@
-import { NextResponse } from 'next/server'
-import { headers } from 'next/headers'
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
-import { stripe } from '../../../lib/stripe'
+import { PLAN_PRICE_ID, stripe } from "../../../lib/stripe";
+import { getUserSession } from "@/lib/core/session";
 // import { stripe } from '@/lib/stripe'
 
-export async function POST() {
+export async function POST(request) {
   try {
-    const headersList = await headers()
-    const origin = headersList.get('origin')
+    const headersList = await headers();
+    const origin = headersList.get("origin");
+    const formData = await request.formData();
+    const planId = formData.get("plan_id");
 
+    const priceId = PLAN_PRICE_ID[planId];
+    const user = await getUserSession();
     // Create Checkout Sessions from body params.
+
     const session = await stripe.checkout.sessions.create({
+      customer_email: user?.email,
       line_items: [
         {
           // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-          price: 'price_1TgRpvLRXVRq2oOW7CXJ6DbA',
+          price: priceId,
           quantity: 1,
         },
       ],
-      mode: 'subscription',
+      mode: "subscription",
       success_url: `${origin}/pricing/success?session_id={CHECKOUT_SESSION_ID}`,
     });
-    return NextResponse.redirect(session.url, 303)
+    return NextResponse.redirect(session.url, 303);
   } catch (err) {
     return NextResponse.json(
       { error: err.message },
-      { status: err.statusCode || 500 }
-    )
+      { status: err.statusCode || 500 },
+    );
   }
 }
--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,5 +1,12 @@
-import 'server-only'
+import "server-only";
 
-import Stripe from 'stripe'
+import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export const PLAN_PRICE_ID = {
+  seeker_pro: "price_1TgRpvLRXVRq2oOW7CXJ6DbA",
+  seeker_premium: "price_1TgTvYLRXVRq2oOWPqWyQboP",
+  recruiter_enterprise: "price_1TgTxgLRXVRq2oOWzigqQoA7",
+  recruiter_growth: "price_1TgTvvLRXVRq2oOWKgZ1fcdk"
+};
